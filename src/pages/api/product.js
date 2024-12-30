76bb0a1db348f138bfc3fbbfea8bb850b6750061
codewithmaryam1418 <maryamahmedkhan78@gmail.com>
2024-12-30 22:32:36 +0500

    first commit

--- src/pages/api/product.js
+++ src/pages/api/product.js
@@ -1,42 +1,42 @@
-export default function handler(req, res) {
+ export default function handler(req, res) {
     const products = [
         {
             id: 1,
             name: "Cheese Burger",
             price: 10,
-            image: "https://www.allrecipes.com/recipe/72657/best-hamburger-ever/",
+            image: "/burger17.webp",
         },
         {
             id: 2,
             name: "Creamy Burger",
             price: 70,
-            image: "https://vegplatter.in/cart/item/add/Cream-Burger/1379",
+            image: "/burger16.jpg",
         },
       
         {
             id: 3,
             name: "Delicious Burger",
             price: 200,
-            image: "https://www.istockphoto.com/photos/delicious-burger",
+            image: "/burger15.jpeg",
         },
      
         {
             id: 4,
             name: "Yummy Burger",
             price: 400,
-            image: "https://www.foodandhome.co.za/recipes/8-yummy-burger-recipes",
+            image: "/burger14.jpg",
         },
         {
             id: 5,
             name: "Yummy Burger",
             price: 500,
-            image: "https://www.timeoutriyadh.com/food-drink/best-burger-in-riyadh-2022",
+            image: "/burger13.jpg",
         },
         {
             id: 6,
             name: "Yummy Burger",
             price: 500,
-            image: "https://torontolife.com/food/torontos-best-cheap-burgers-right-now/",
+            image: "/burger12.jpg",
         },
     ];
 
